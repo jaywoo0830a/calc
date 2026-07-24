df8c1f4fb556a7625d5cdcea3a0d4f851b6dfa62
= <=>
2026-07-24 11:31:17 +0900

    remove proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './pages/Calculator.jsx';
 import Viewer from './pages/Viewer.jsx';
-import Proxy from './pages/Proxy.jsx';
 
 export default function App() {
   return (
@@ -9,7 +8,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Calculator />} />
         <Route path="/viewer" element={<Viewer />} />
-        <Route path="/proxy" element={<Proxy />} />
       </Routes>
     </BrowserRouter>
   );
